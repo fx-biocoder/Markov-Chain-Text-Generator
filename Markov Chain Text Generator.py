dc786902f0425c42fd8064347c4522f651b30fb1
Facundo Martínez <90737264+fx-biocoder@users.noreply.github.com>
2021-10-09 15:39:54 -0300

    Modificación menor
    Eliminé las notas iniciales del script ya que se encuentran actualmente en el archivo README.md

--- Markov Chain Text Generator.py
+++ Markov Chain Text Generator.py
@@ -1,24 +1,3 @@
-#   GENERADOR DE TEXTO MEDIANTE CADENAS DE MARKOV
-#   Proyecto basado en el script realizado por Jeffrey Carpenter (https://www.linkedin.com/in/jeffcarp/)
-
-#   Highlights:
-#   1. Uso de la librería easygui para seleccionar el archivo fuente de manera manual
-#       Esto permite seleccionar fácilmente el archivo fuente sin necesidad de escribir la ruta de archivo
-#   2. Sintaxis más compacta y legible
-#   3. Posibilidad de seleccionar el largo y número de sentencias generadas
-
-#   Pueden probar este script usando el archivo "stallman_copypasta.txt" presente en este mismo repositorio
-
-"""
-    === ¿QUÉ ES UNA CADENA DE MARKOV? ===
-    
-        Una cadena de Markov es un sistema en un tiempo discreto, que en cada instante se encuentra en un estado distinto. 
-        La cantidad total de estados es finita, y en cada instante se pasa de un estado a otro con unas probabilidades fijas 
-        que dependen sólo del estado actual, y no del resto de la historia del sistema.
-        (Definición extraída de http://matematicas.uam.es/~pablo.angulo/markov/markov.html#definicion-de-cadena-de-markov)
-        
-"""
-
 #   1 - librerías necesarias para el proyecto
 #from tkinter.constants import FALSE, TRUE
 import easygui as eg, numpy as np, random, re
